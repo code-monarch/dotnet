faea285a9c23ca8825af93ed3d3c8cb0737b4218
code-monarch <adiejoel14@gmail.com>
2024-03-11 14:43:17 +0100

    lskdljk

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -49,8 +49,7 @@ namespace NZWalks.API.Controllers
 
             return BadRequest("Something went wrong");
         }
-
-
+        
         // POST: /api/Auth/Login
         [HttpPost]
         [Route("Login")]
